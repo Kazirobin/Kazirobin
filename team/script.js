993625e9fdabdad606a10b8d1d2e21b11fb209f0
Kazi Robin <kazirobinking@gmail.com>
2023-11-27 23:27:24 +0600

    up

---
+++ team/script.js
@@ -0,0 +1,76 @@
+const originalData = [
+  {
+    image: "./img/parson/Prince_Parves.jpg",
+    title: "Prince_Parves",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+    social: {
+      facebook: {
+        url: "https://www.facebook.com/prince.parves.946",
+        icon: "./img/social_icon/facebook.svg",
+      },
+      messenger: {
+        url: "https://www.facebook.com/messages/t/100015046562328",
+        icon: "./img/social_icon/messenger.svg",
+      },
+      github: {
+        url: "https://www.facebook.com/?ref=homescreenpwa",
+        icon: "./img/social_icon/github.svg",
+      },
+      whatsapp: {
+        url: "tel:+8801795209494",
+        icon: "./img/social_icon/whatsapp.svg",
+      },
+    },
+    tags: ["developer", "Prince_Parves"],
+  },
+  {
+    image: "./img/parson/Saheb_Ali.jpg",
+    title: "Saheb_Ali",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+    social: {
+      facebook: {
+        url: "https://www.facebook.com/prince.parves.946",
+        icon: "./img/social_icon/facebook.svg",
+      },
+      messenger: {
+        url: "https://www.facebook.com/messages/t/100015046562328",
+        icon: "./img/social_icon/messenger.svg",
+      },
+      github: {
+        url: "https://www.facebook.com/?ref=homescreenpwa",
+        icon: "./img/social_icon/github.svg",
+      },
+      whatsapp: {
+        url: "tel:+8801795209494",
+        icon: "./img/social_icon/whatsapp.svg",
+      },
+    },
+    tags: ["learner", "Saheb_Ali"],
+  },
+  {
+    image: "./img/parson/Shekh_Rasel_Masrur_Ahmmad.jpg",
+    title: "Shekh_Rasel_Masrur_Ahmmad",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+    social: {
+      facebook: {
+        url: "https://www.facebook.com/prince.parves.946",
+        icon: "./img/social_icon/facebook.svg",
+      },
+      messenger: {
+        url: "https://www.facebook.com/messages/t/100015046562328",
+        icon: "./img/social_icon/messenger.svg",
+      },
+      github: {
+        url: "https://www.facebook.com/?ref=homescreenpwa",
+        icon: "./img/social_icon/github.svg",
+      },
+      whatsapp: {
+        url: "tel:+8801795209494",
+        icon: "./img/social_icon/whatsapp.svg",
+      },
+    },
+    tags: ["developer", "Shekh_Rasel_Masrur_Ahmmad"],
+  },
+
+  // ... other objects
+];
