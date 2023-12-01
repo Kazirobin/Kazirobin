a42be502876a4f134616c8f7def5f6765e5a5e07
Kazi Robin <kazirobinking@gmail.com>
2023-12-01 15:28:54 +0600

    update

---
+++ assets/js/team_database.js
@@ -0,0 +1,103 @@
+const originalData = [
+    {
+      image: "./assets/img/parson/Prince_Parves.jpg",
+      title: "Prince_Parves",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+      social: {
+        facebook: {
+          url: "https://www.facebook.com/prince.parves.946",
+          icon: "./assets/img/social_icon/facebook.svg",
+        },
+        messenger: {
+          url: "https://www.facebook.com/messages/t/100015046562328",
+          icon: "./assets/img/social_icon/messenger.svg",
+        },
+        github: {
+          url: "https://www.facebook.com/?ref=homescreenpwa",
+          icon: "./assets/img/social_icon/github.svg",
+        },
+        whatsapp: {
+          url: "tel:+8801795209494",
+          icon: "./assets/img/social_icon/whatsapp.svg",
+        },
+      },
+      tags: ["developer", "Prince_Parves"],
+    },
+    {
+      image: "./assets/img/parson/Saheb_Ali.jpg",
+      title: "Saheb_Ali",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+      social: {
+        facebook: {
+          url: "https://www.facebook.com/prince.parves.946",
+          icon: "./assets/img/social_icon/facebook.svg",
+        },
+        messenger: {
+          url: "https://www.facebook.com/messages/t/100015046562328",
+          icon: "./assets/img/social_icon/messenger.svg",
+        },
+        github: {
+          url: "https://www.facebook.com/?ref=homescreenpwa",
+          icon: "./assets/img/social_icon/github.svg",
+        },
+        whatsapp: {
+          url: "tel:+8801795209494",
+          icon: "./assets/img/social_icon/whatsapp.svg",
+        },
+      },
+      tags: ["learner", "Saheb_Ali"],
+    },
+    {
+      image: "./assets/img/parson/Shekh_Rasel_Masrur_Ahmmad.jpg",
+      title: "Shekh_Rasel_Masrur_Ahmmad",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+      social: {
+        facebook: {
+          url: "https://www.facebook.com/prince.parves.946",
+          icon: "./assets/img/social_icon/facebook.svg",
+          
+        },
+        messenger: {
+          url: "https://www.facebook.com/messages/t/100015046562328",
+          icon: "./assets/img/social_icon/messenger.svg",
+        },
+        github: {
+          url: "https://www.facebook.com/?ref=homescreenpwa",
+          icon: "./assets/img/social_icon/github.svg",
+        },
+        whatsapp: {
+          url: "tel:+8801795209494",
+          icon: "./assets/img/social_icon/whatsapp.svg",
+        },
+      },
+      tags: ["developer", "Shekh_Rasel_Masrur_Ahmmad"],
+    },    
+    {
+      image: "./assets/img/parson/Rauf_Hossain.jpg",
+      title: "Rauf_Hossain",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum labore quae veniam reiciendis cum necessitatibus dolorem voluptate quod!",
+      social: {
+        facebook: {
+          url: "https://www.facebook.com/RaufHossain.dev",
+          icon: "./assets/img/social_icon/facebook.svg",
+        },
+        messenger: {
+          url: "https://www.facebook.com/messages/t/100008436814841",
+          icon: "./assets/img/social_icon/messenger.svg",
+        },
+        github: {
+          url: "https://github.com/dev-raufhossain",
+          icon: "./assets/img/social_icon/github.svg",
+        },
+        whatsapp: {
+          
+          url: "tel:+8801303059731",
+          icon: "./assets/img/social_icon/whatsapp.svg",
+        },
+      },
+      tags: ["developer", "Rauf_Hossain"],
+    },
+  
+    // ... other objects
+  ];
+  
